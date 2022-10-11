85c0eb5b829cfe02b94d60af0528eb3fea5d4432
Gaurav Subhash Yawale <gauravs@geekyants.com>
2022-10-11 16:54:20 +0530

    fix/RadioButtonGroup: fixed default state of RadioButtonGroup by adding initialValue prop

--- src/components/app/radioButtonGroup/RadioButtonGroup.tsx
+++ src/components/app/radioButtonGroup/RadioButtonGroup.tsx
@@ -4,8 +4,10 @@ import type { RadioButtonGroupProps } from './types';
 import './style.css';
 
 export const RadioButtonGroup = (props: RadioButtonGroupProps): JSX.Element => {
-  const { list, onChange, size, space } = props;
-  const [selectedValue, setSelectedValue] = useState<string>('');
+  const { list, onChange, size, space, initialValue } = props;
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    initialValue
+  );
 
   const handleButtonSelection = (value: string) => {
     setSelectedValue(value);
@@ -13,20 +15,24 @@ export const RadioButtonGroup = (props: RadioButtonGroupProps): JSX.Element => {
   };
 
   const createButtons = () => {
-    const radioButtons = list?.map((value: string, index: number) => {
-      return (
-        <div key={index}>
-          <label
-            className={`button-common ${
-              value === selectedValue ? 'button-selected' : 'button-un-selected'
-            } button-${size ? size : 'medium'}`}
-          >
-            {value}
-            <Radio value={value} marginLeft="30px" />
-          </label>
-        </div>
-      );
-    });
+    const radioButtons =
+      list?.length &&
+      list?.map((value: string, index: number) => {
+        return (
+          <div key={`${value}_${index}`}>
+            <label
+              className={`button-common ${
+                value === selectedValue
+                  ? 'button-selected'
+                  : 'button-un-selected'
+              } button-${size ? size : 'medium'}`}
+            >
+              {value}
+              <Radio value={value} marginLeft="30px" />
+            </label>
+          </div>
+        );
+      });
 
     return radioButtons;
   };
@@ -47,9 +53,10 @@ export const RadioButtonGroup = (props: RadioButtonGroupProps): JSX.Element => {
 };
 
 RadioButtonGroup.DefaultProps = {
-  list: ['Employer', 'Candidate'],
+  list: [],
   onChange: undefined,
   size: 'medium',
   label: 'RadioButtonGroup',
   sapce: '20.5px',
+  initialValue: undefined,
 };
--- src/components/app/radioButtonGroup/types.ts
+++ src/components/app/radioButtonGroup/types.ts
@@ -4,6 +4,7 @@
  * @param size - is for managing size of radio button, "medium" as default e.g, size="medium" or size="small"
  * @param label - is for adding label to Radio Button group, "RadioButtonGroup" as default.
  * @param space - is for adding space between the radio buttons, "20.5px" as default. e.g, space="10px"
+ * @param initialValue - is for setting up initial selection of radio button e.g, initialValue="Employer"
  */
 
 export type RadioButtonGroupProps = {
@@ -12,4 +13,5 @@ export type RadioButtonGroupProps = {
   size: 'extraLarge' | 'large' | 'medium' | 'small';
   label: string;
   space: string;
+  initialValue: string | undefined;
 };
