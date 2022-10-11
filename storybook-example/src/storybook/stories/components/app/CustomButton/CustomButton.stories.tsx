465fa585e654bcc98f9298085a3f1fdbdc24d099
Gaurav Subhash Yawale <gauravs@geekyants.com>
2022-10-11 19:59:43 +0530

    :rocket:feat/CustomButton: added CustomButton feature to storybook

---
+++ storybook-example/src/storybook/stories/components/app/CustomButton/CustomButton.stories.tsx
@@ -0,0 +1,19 @@
+import { Wrapper } from '../../Wrapper';
+import { StorybookArgs } from './CustomButton.args';
+import React from 'react';
+import { CustomButton } from 'components';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'CustomButton',
+  component: CustomButton,
+} as ComponentMeta<typeof CustomButton>;
+
+const Template: ComponentStory<typeof CustomButton> = (args) => (
+  <Wrapper>
+    <CustomButton {...args}>Employer</CustomButton>
+  </Wrapper>
+);
+export const Variants = Template.bind({});
+
+Variants.args = StorybookArgs;
