ec63ecae43a7e16157224b9e7836543348e2c4aa
Gaurav Subhash Yawale <gauravs@geekyants.com>
2022-10-11 15:03:26 +0530

    feat/RadioButtonGroup: created RadioButtonGroup Component

---
+++ src/components/app/radioButtonGroup/RadioButtonGroup.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Radio, HStack, VStack } from 'native-base';
+import type { RadioButtonGroupProps } from './types';
+import './style.css';
+
+export const RadioButtonGroup = (props: RadioButtonGroupProps): JSX.Element => {
+  const { list, onChange, size, space } = props;
+  const [selectedValue, setSelectedValue] = useState<string>('');
+
+  const handleButtonSelection = (value: string) => {
+    setSelectedValue(value);
+    if (onChange) onChange(value);
+  };
+
+  const createButtons = () => {
+    const radioButtons = list?.map((value: string, index: number) => {
+      return (
+        <div key={index}>
+          <label
+            className={`button-common ${
+              value === selectedValue ? 'button-selected' : 'button-un-selected'
+            } button-${size ? size : 'medium'}`}
+          >
+            {value}
+            <Radio value={value} />
+          </label>
+        </div>
+      );
+    });
+
+    return radioButtons;
+  };
+
+  return (
+    <Radio.Group
+      name="radioButtonGroup"
+      onChange={handleButtonSelection}
+      value={selectedValue}
+    >
+      {size === 'extraLarge' ? (
+        <VStack space={space}>{createButtons()}</VStack>
+      ) : (
+        <HStack space={space}>{createButtons()}</HStack>
+      )}
+    </Radio.Group>
+  );
+};
+
+RadioButtonGroup.DefaultProps = {
+  list: ['Employer', 'Candidate'],
+  onChange: undefined,
+  size: 'medium',
+  label: 'RadioButtonGroup',
+  sapce: '20.5px',
+};
---
+++ src/components/app/radioButtonGroup/types.ts
@@ -0,0 +1,15 @@
+/**
+ * @param onChange - is for getting selected radio button which returns string, "undefined" as default e.g, selected radio button is "Employer", onChange = (value:string)=>console.log(value) // output: "Employer"
+ * @param list - is for adding radio buttons,  e.g, list = ["Candidate", "Employer"] as default
+ * @param size - is for managing size of radio button, "medium" as default e.g, size="medium" or size="small"
+ * @param label - is for adding label to Radio Button group, "RadioButtonGroup" as default.
+ * @param space - is for adding space between the radio buttons, "20.5px" as default. e.g, space="10px"
+ */
+
+export type RadioButtonGroupProps = {
+  onChange?: (value: string) => void;
+  list: string[];
+  size: 'extraLarge' | 'large' | 'medium' | 'small';
+  label: string;
+  space: string;
+};
