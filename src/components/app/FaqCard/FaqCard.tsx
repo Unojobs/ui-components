1187540535d76167e060c6ad56f25bd1c11f246a
Manoj Kumar M <manojkumar@geekyants.com>
2022-10-13 17:55:37 +0530

    WIP: style, import changes

--- src/components/app/FaqCard/FaqCard.tsx
+++ src/components/app/FaqCard/FaqCard.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Accordion } from 'src/components/composites/Accordion/Accordion';
-import { AccordionDetails } from 'src/components/composites/Accordion/AccordionDetails';
-import { AccordionIcon } from 'src/components/composites/Accordion/AccordionIcon';
-import { AccordionItem } from 'src/components/composites/Accordion/AccordionItem';
-import { AccordionSummary } from 'src/components/composites/Accordion/AccordionSummary';
-import { Box, Text } from 'src/components/primitives';
+import { Accordion } from '../../composites';
+import { Text } from '../../primitives';
 import type { FaqCardProps } from './types';
 
 export const FaqCard = (props: FaqCardProps): JSX.Element => {
@@ -15,10 +11,10 @@ export const FaqCard = (props: FaqCardProps): JSX.Element => {
       maxW="100%"
       bgColor="#fafafa"
       borderWidth={0}
-      borderRadius="md"
+      borderRadius="lg"
     >
-      <AccordionItem>
-        <AccordionSummary
+      <Accordion.Item>
+        <Accordion.Summary
           _hover={{
             bgColor: '#fafafa',
           }}
@@ -27,14 +23,18 @@ export const FaqCard = (props: FaqCardProps): JSX.Element => {
           }}
         >
           <Text fontWeight={600}>{title}</Text>
-          <Box bgColor="#4169E069" p={1} borderRadius="full">
-            <AccordionIcon color="#4169E0" />
-          </Box>
-        </AccordionSummary>
-        <AccordionDetails>
+          <Accordion.Icon
+            bgColor="#4169e066"
+            color="#4169e0"
+            borderRadius="full"
+            p="1"
+            size="6"
+          />
+        </Accordion.Summary>
+        <Accordion.Details>
           <Text color="#707070">{description}</Text>
-        </AccordionDetails>
-      </AccordionItem>
+        </Accordion.Details>
+      </Accordion.Item>
     </Accordion>
   );
 };
