25a7400e9ccb7536ff7cb1eff103ec39d2442539
Viraj-10 <virajjoshi132@gmail.com>
2022-10-21 16:31:00 +0530

    feat: adding break points

--- src/components/core/AppProvider.tsx
+++ src/components/core/AppProvider.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { NativeBaseProvider, extendTheme } from 'native-base';
+import { NativeBaseProvider, extendTheme, Box } from 'native-base';
 import { theme as defaultTheme } from '../../theme';
 import { custom } from '../../theme/custom';
-
-export const appTheme = extendTheme(defaultTheme, custom);
+import merge from 'lodash.merge';
+export const appTheme = extendTheme(merge(defaultTheme, custom));
+type MyThemeType = typeof appTheme;
+declare module 'native-base' {
+  interface ICustomTheme extends MyThemeType {}
+}
 export const AppProvider = ({ children, theme, ...props }: any) => {
   return (
     <NativeBaseProvider theme={extendTheme(appTheme, theme)} {...props}>
+      <Box></Box>
       {children}
     </NativeBaseProvider>
   );
--- src/theme/custom.ts
+++ src/theme/custom.ts
@@ -1,1 +1,10 @@
-export const custom = {};
+export const custom = {
+  breakpoints: {
+    base: 0,
+    xs: 576,
+    sm: 768,
+    md: 992,
+    lg: 1200,
+    xl: 1600,
+  },
+};
