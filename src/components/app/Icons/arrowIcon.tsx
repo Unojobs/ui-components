35537d50b61dab7949873af297372ef1a5e8934a
Gaurav Subhash Yawale <gauravs@geekyants.com>
2022-10-28 20:29:39 +0530

    🔨fix: icon properties

--- src/components/app/Icons/arrowIcon.tsx
+++ src/components/app/Icons/arrowIcon.tsx
@@ -67,3 +67,35 @@ const DownCurvedSvg = () => (
 export const DownArrowCurvedIcon = (
   props: Partial<CustomIconComponentProps>
 ) => <Icon component={DownCurvedSvg} {...props} />;
+
+/** BackArrowSVG for sidemenu to expand items */
+const BackArrowSVG = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.57 5.92993L3.5 11.9999L9.57 18.0699"
+      stroke="#000000"
+      strokeWidth="2.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M40.5019 12H3.67188"
+      stroke="#000000"
+      strokeWidth="2.5"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const BackArrowIcon = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={BackArrowSVG} {...props} />
+);
