1299d7b25f29c719360d9fad32a646f89ce56ca5
utsavo-afk <utsavo@geekyants.com>
2022-11-21 14:35:19 +0530

    fix(messages): 🛠 FIX name conflict in TabStyleWrapper component

--- src/components/app/EmployerDashboard/Messages/EmployerTabStyleWrapper/EmployerTabStyleWrapper.tsx
+++ src/components/app/EmployerDashboard/Messages/EmployerTabStyleWrapper/EmployerTabStyleWrapper.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './TabStyleWrapper.css';
 
-export const TabStyleWrapper = (props: { children: React.ReactNode }) => {
+export const EmployerTabStyleWrapper = (props: {
+  children: React.ReactNode;
+}) => {
   const { children, ...rest } = props;
   return (
     <div {...rest} className="tabs-antd-custom-styles-employer-portal">
