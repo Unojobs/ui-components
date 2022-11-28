3bcda32dd43d87dbfad393e7f7b39dd15ddeea61
Viraj-10 <virajjoshi132@gmail.com>
2022-11-28 23:26:31 +0530

    fix: hidden ssr issue from and to

--- src/components/primitives/Hidden/Hidden.web.tsx
+++ src/components/primitives/Hidden/Hidden.web.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+const breakPoints = ['base', 'sm', 'md', 'lg', 'xl'];
 const Hidden = React.memo(({ children, ...props }: any) => {
   const { from, till, only } = props;
 
   let display: any = {};
 
   if (till) {
-    display = { base: 'none', [till]: 'flex' };
+    for (const i in breakPoints) {
+      if (breakPoints[i] === till) {
+        display[breakPoints[i]] = 'none';
+      } else {
+        display[breakPoints[i]] = 'flex';
+      }
+    }
   }
   if (from) {
-    display = { base: 'flex', [from]: 'flex' };
+    let flag = false;
+    for (const i in breakPoints) {
+      if (breakPoints[i] === from || flag) {
+        display[breakPoints[i]] = 'none';
+        flag = true;
+      } else {
+        display[breakPoints[i]] = 'flex';
+      }
+    }
   }
   if (only) {
     display.base = 'flex';
