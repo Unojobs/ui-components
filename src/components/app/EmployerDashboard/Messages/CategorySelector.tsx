37ea1b68f270d9220b0edc40cbf7c4fafc9a9564
Adeel Faridi <adeelf@geekyants.com>
2022-11-29 12:52:10 +0530

    fix: 🔨 refactor code and fix UI

--- src/components/app/EmployerDashboard/Messages/CategorySelector.tsx
+++ src/components/app/EmployerDashboard/Messages/CategorySelector.tsx
@@ -2,23 +2,44 @@ import React from 'react';
 import { Select } from '../../../primitives';
 
 // MessagesSidebar > Select dropdown
+
+interface ListType {
+  label: string;
+  value: string;
+}
 export interface CategorySelectorProps {
   defaultValue: string;
   placeholder: string;
   onValueChange: (value: string) => void;
+  list: Array<ListType>;
 }
+
+const STATIC_PROPS = {
+  variant: 'outline',
+  fontWeight: 400,
+  lineHeight: '140%',
+  letterSpacing: 0.16,
+  fontSize: 'fontSizes.biggersm',
+  selectedTextColor: 'textColors.primary',
+} as const;
+
 export const CategorySelector = (props: CategorySelectorProps) => {
-  const { defaultValue, placeholder, onValueChange } = props;
+  const { defaultValue, placeholder, onValueChange, list } = props;
   return (
     <Select
-      variant={'outline'}
-      fontSize={'lg'}
       defaultValue={defaultValue}
       placeholder={placeholder}
       onValueChange={onValueChange}
+      {...STATIC_PROPS}
     >
-      <Select.Item label="Projects" value="projects" />
-      <Select.Item label="Candidates" value="candidates" />
+      {list.map((obj) => (
+        <Select.Item label={obj.label} value={obj.value} />
+      ))}
     </Select>
   );
+};
+
+CategorySelector.defaultProps = {
+  list: [],
+  defaultValue: 'Projects',
 };
