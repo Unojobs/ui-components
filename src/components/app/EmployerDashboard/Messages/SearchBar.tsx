b415fbe97c456b35838128797d720f8d9095a05a
Adeel Faridi <adeelf@geekyants.com>
2022-11-29 12:52:38 +0530

    fix: 🔨 refactor code and fix UI

--- src/components/app/EmployerDashboard/Messages/SearchBar.tsx
+++ src/components/app/EmployerDashboard/Messages/SearchBar.tsx
@@ -5,27 +5,34 @@ export interface SearchBarProps {
   value: string;
   onChangeText: (text: string) => void;
 }
+const STATIC_PROPS = {
+  CONTAINER_BOX: {
+    justifyContent: 'center',
+    height: '43px',
+    borderStyle: 'solid',
+    borderColor: 'light.300',
+    borderWidth: 1,
+    borderRadius: 'sm',
+    paddingX: 5,
+    paddingY: '12.5px',
+  },
+  INPUT: {
+    InputRightElement: <SearchIcon size={5} />,
+    variant: 'unstyled',
+    fontSize: 14,
+    placeholder: 'Search Messages',
+  },
+} as const;
+
 export const SearchBar = (props: SearchBarProps) => {
   const { value, onChangeText } = props;
   return (
-    <Box
-      justifyContent={'center'}
-      height={'43px'}
-      borderStyle={'solid'}
-      borderColor={'light.300'}
-      borderWidth={'1'}
-      borderRadius={'sm'}
-    >
-      <Box marginRight={'2'}>
-        <Input
-          placeholder="Search Messages"
-          InputRightElement={<SearchIcon size={'lg'} />}
-          variant={'unstyled'}
-          fontSize={'md'}
-          onChangeText={onChangeText}
-          value={value}
-        />
-      </Box>
+    <Box {...STATIC_PROPS.CONTAINER_BOX}>
+      <Input
+        onChangeText={onChangeText}
+        value={value}
+        {...STATIC_PROPS.INPUT}
+      />
     </Box>
   );
 };
