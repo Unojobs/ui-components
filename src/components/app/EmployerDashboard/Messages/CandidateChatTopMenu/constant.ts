4cd72d32524f6f567c11ab46c3a9fb23c02a34be
Adeel Faridi <adeelf@geekyants.com>
2022-11-30 05:05:16 +0530

    fix: top menu and chat menu

---
+++ src/components/app/EmployerDashboard/Messages/CandidateChatTopMenu/constant.ts
@@ -0,0 +1,41 @@
+export const STATIC_PROPS = {
+  CONTAINER: {
+    px: '22px',
+    pt: 4,
+    pb: 2.5,
+    width: 'full',
+    height: 'full',
+    space: 12,
+    borderColor: 'secondary.400',
+    borderWidth: 1,
+    overflow: 'scroll',
+  },
+  PROFILE: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 'full',
+    minWidth: '700px',
+  },
+  FONT_14_SEMIBOLD: {
+    fontSize: '14px',
+    fontWeight: 500,
+  },
+  FONT_16_SEMIBOLD: {
+    fontSize: 'md',
+    fontWeight: 500,
+  },
+  FONT_12_SEMIBOLD: {
+    fontSize: 'xs',
+    fontWeight: 500,
+  },
+  CENTER_SPACE_12: {
+    alignItems: 'center',
+    space: 3,
+  },
+  FIT_CONTENT_TRANSPARENT: {
+    width: 'fit-content',
+    height: 'fit-content',
+    padding: '0',
+    backgroundColor: 'transparent',
+  },
+};
