4badddb56a4d62d7c3c49ca2abf7bad290a6bc83
Viraj-10 <virajjoshi132@gmail.com>
2022-12-05 13:40:52 +0530

    fix: appprovider theme

--- src/components/core/AppProvider.tsx
+++ src/components/core/AppProvider.tsx
@@ -3,11 +3,13 @@ import { NativeBaseProvider, extendTheme } from 'native-base';
 import { theme as defaultTheme } from '../../theme';
 import { custom } from '../../theme/custom';
 import merge from 'lodash.merge';
-export const appTheme = extendTheme(merge({ ...defaultTheme }, { ...custom }));
+// export const appTheme = extendTheme(merge({ ...defaultTheme }, { ...custom }));
 export const AppProvider = ({ children, theme, ...props }: any) => {
   return (
     <NativeBaseProvider
-      theme={extendTheme(defaultTheme, custom, theme)}
+      theme={extendTheme(
+        merge({ ...defaultTheme }, { ...custom }, { ...theme })
+      )}
       {...props}
     >
       {children}
