60e5ce26ff25ff15646eb51994ca7c3b2910d106
Ankur Rathi <ankurrathi@geekyants.com>
2023-01-23 15:17:40 +0530

    fix: typo error

--- src/components/app/UnojobsIcons/pipeline.tsx
+++ src/components/app/UnojobsIcons/pipeline.tsx
@@ -13,43 +13,43 @@ export const MessagesRoundIcon = ({ size }: IconProps) => {
         <path
           d="M11.9854 7.19334V9.86001C11.9854 10.0333 11.9787 10.2 11.9587 10.36C11.8054 12.16 10.7454 13.0533 8.79203 13.0533H8.52537C8.3587 13.0533 8.19869 13.1333 8.09869 13.2667L7.29871 14.3333C6.94537 14.8067 6.37203 14.8067 6.0187 14.3333L5.21869 13.2667C5.13202 13.1533 4.9387 13.0533 4.79203 13.0533H4.52537C2.3987 13.0533 1.33203 12.5267 1.33203 9.86001V7.19334C1.33203 5.24001 2.23204 4.18001 4.02537 4.02667C4.18537 4.00667 4.35204 4 4.52537 4H8.79203C10.9187 4 11.9854 5.06667 11.9854 7.19334Z"
           stroke="#B7B7B7"
-          stroke-width="1.5"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           fill="none"
         />
         <path
           d="M14.654 4.52537V7.19204C14.654 9.15204 13.754 10.2054 11.9607 10.3587C11.9807 10.1987 11.9873 10.032 11.9873 9.8587V7.19204C11.9873 5.06537 10.9207 3.9987 8.79401 3.9987H4.52734C4.35401 3.9987 4.18734 4.00537 4.02734 4.02537C4.18068 2.23204 5.24068 1.33203 7.19401 1.33203H11.4607C13.5873 1.33203 14.654 2.3987 14.654 4.52537Z"
           stroke="#B7B7B7"
-          stroke-width="1.5"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           fill="none"
         />
         <path
           d="M8.9983 8.83333H9.0043"
           stroke="#B7B7B7"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           fill="none"
         />
         <path
           d="M6.66236 8.83333H6.66836"
           stroke="#B7B7B7"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           fill="none"
         />
         <path
           d="M4.33033 8.83333H4.33633"
           stroke="#B7B7B7"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           fill="none"
         />
       </>
